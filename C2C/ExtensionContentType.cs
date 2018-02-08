1fde859c6981ecee7e88e328c868ef23ca746cf0
EkaSe <ekasemyash@ya.ru>
2018-02-09 00:16:03 +0300

    #5 comments

--- C2C/ExtensionContentType.cs
+++ C2C/ExtensionContentType.cs
@@ -3,23 +3,31 @@ using System.Collections.Generic;
 
 namespace C2C
 {
-    internal class ExtensionContentType
+    internal sealed class ExtensionContentType
     {
-        private static Dictionary<string, string> types = new Dictionary<string, string>
+        private static Dictionary<string, string> _types;
+
+        private static Dictionary<string, string> types 
         {
-                {".html", "text/HTML"},
-                {".css", "text/plain"},
-                {".js", "text/plain"},
-                {".jpeg", "image/JPEG"},
-                {".jpg", "image/JPEG"}
-        };
-       
-        public string this[string extension] 
-        { 
-            get => 
-                types.ContainsKey(extension) 
-                ? types[extension] 
-                : "text/plain"; 
+            get 
+            {
+                if (_types is null) 
+                {
+                    _types = new Dictionary<string, string>
+                    {
+                            {".html", "text/HTML"},
+                            {".css", "text/plain"},
+                            {".js", "text/plain"},
+                            {".jpeg", "image/JPEG"},
+                            {".jpg", "image/JPEG"}
+                    };
+                }
+                return _types;
+            }
         }
+       
+        public string this[string extension] => types.ContainsKey(extension) 
+                    ? types[extension]
+                    : "text/plain"; 
     }
 }
